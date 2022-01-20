2e456cc45b10ff38c8066bf17b0a5a2b69cd71a3
micha <michael.mcarthur@hotmail.co.uk>
2022-01-20 13:24:02 +0000

    function to change datatypes for entities and function to split genre file into a list

---
+++ imdbProject/cleanData.py
@@ -0,0 +1,19 @@
+import extractSortCSV
+
+
+def change_data_type():
+    df = extractSortCSV.complete_data()
+    df = df.astype({'year': 'int64'})
+    df = df.astype({'duration': 'int64'})
+    df = df.astype({'budget': 'int64'})
+    df = df.astype({'gross': 'int64'})
+    # df['year'] = pd.to_datetime(df['year'], format='%Y')
+    return df
+
+
+def genre_to_list():
+    df = change_data_type()
+    for index, row in df.iterrows():
+        genres = row.genres.split('|')
+        df.at[index, 'genres'] = genres
+    return df
