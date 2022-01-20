43eef5aeabdd6c09acf7bfc0b248a896a744f7d5
micha <michael.mcarthur@hotmail.co.uk>
2022-01-20 13:26:23 +0000

    edited imports after name change

--- tests/test_extractCSV.py
+++ tests/test_extractCSV.py
@@ -1,12 +1,4 @@
-from context import extractCSV
+from context import extractSortCSV
 
 # rows in full data
-print(len(extractCSV.read_csv().index))
-# rows in complete data
-print(len(extractCSV.complete_data().index))
-# rows in partial data
-print(len(extractCSV.part_data().index))
-# rows in partial data with drop na (should be 0)
-print(len(extractCSV.part_data().dropna().index))
-# rows in the sum of complete data and partial data
-print(len(extractCSV.complete_data().index) + len(extractCSV.part_data().index))
+print(len(extractSortCSV.read_csv().index))
